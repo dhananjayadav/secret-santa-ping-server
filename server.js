68f49e429d0666eafa30c428d428fcd91bc7b8fe
Priyanshi Yadav <dhananjay2451@gmail.com>
2025-04-06 00:28:41 +0530

    update server fil

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const SECRET_SENTA_INTERVAL = 5 * 60000; // 5 mins
+const SECRET_SENTA_INTERVAL = 5 * 60000;
 
 const secretSantaPing = async () => {
-    console.log("PING-SECRET-SANTA")
-    const response = await axios.post(`${process.env.SECRET_SENTA_SERVER_BASE_URL}/api/secret-santa/assign`);
+  console.log("PING-SECRET-SANTA");
+  try {
+    const response = await axios.post(
+      `${process.env.SECRET_SANTA_SERVER_BASE_URL}/api/secret-santa/assign`
+    );
     console.log({ msg: "SECRET_SANTA", response });
-}
+  } catch (err) {
+    console.log({ msg: "SECRET_SANTA", err: err.mesage });
+  }
+};
 
 setInterval(() => {
-    secretSantaPing()
+  secretSantaPing();
 }, SECRET_SENTA_INTERVAL);
 
-app.listen(PORT, ()=> {
-    console.log(`Server running on PORT: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
